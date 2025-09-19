50123f5e7b9dc81db79f6ada88f5589f69ec8cab
XiaoYingYo <759852125@qq.com>
2025-09-19 11:26:09 +0800

    no message

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -4,11 +4,9 @@ class Tab {
     this.id = `tab-${Date.now()}`;
     this.history = [];
     this.historyIndex = -1;
-
     this.createElement();
     this.createContentElement();
   }
-
   createElement() {
     this.element = document.createElement('div');
     this.element.className = 'flex items-center bg-[#2d2d2d] px-3 py-2 border-r border-gray-700';
@@ -20,35 +18,29 @@ class Tab {
         <span class="material-icons text-xs">close</span>
       </button>
     `;
-
     this.element.addEventListener('click', () => this.tabManager.switchTab(this.id));
     this.element.querySelector('.close-tab-button').addEventListener('click', (e) => {
       e.stopPropagation();
       this.tabManager.closeTab(this.id);
     });
   }
-
   createContentElement() {
     this.contentElement = document.createElement('div');
     this.contentElement.id = this.id;
     this.contentElement.className = 'p-4 h-full';
     this.contentElement.style.display = 'none';
   }
-
   setTitle(title) {
     this.element.querySelector('.text-sm').textContent = title;
   }
-
   show() {
     this.element.classList.add('bg-[#2d2d2d]');
     this.contentElement.style.display = 'block';
   }
-
   hide() {
     this.element.classList.remove('bg-[#2d2d2d]');
     this.contentElement.style.display = 'none';
   }
-
   async loadPath(path) {
     if (this.history[this.historyIndex] === path) {
       return;
@@ -59,7 +51,6 @@ class Tab {
       await this.loadFiles(path);
     }
   }
-
   async goBack() {
     if (this.historyIndex > 0) {
       this.historyIndex--;
@@ -72,7 +63,6 @@ class Tab {
       this.tabManager.updateNavigationButtons();
     }
   }
-
   async goForward() {
     if (this.historyIndex < this.history.length - 1) {
       this.historyIndex++;
@@ -85,19 +75,16 @@ class Tab {
       this.tabManager.updateNavigationButtons();
     }
   }
-
   async loadDisks(addToHistory = true) {
     try {
       this.setTitle('此电脑');
       this.tabManager.setPathInputValue('此电脑');
       const [disksResponse, diskTemplateResponse] = await Promise.all([fetch('/api/disks'), fetch('./tpl/viewMode/disk.html')]);
       if (!disksResponse.ok || !diskTemplateResponse.ok) throw new Error('Failed to load disk data or template');
-
       const disks = await disksResponse.json();
       const diskViewTemplate = await diskTemplateResponse.text();
       const diskItemTemplateMatch = diskViewTemplate.match(/<for>([\s\S]*?)<\/for>/);
       if (!diskItemTemplateMatch) throw new Error('Disk item template not found');
-
       const diskItemTemplate = diskItemTemplateMatch[1].trim();
       let allDisksHtml = disks
         .map((disk) => {
@@ -106,7 +93,6 @@ class Tab {
           return diskItemTemplate.replace('{{diskType}}', disk.type).replace('{{diskPath}}', disk.path.slice(0, 2)).replace('{{usedPercentage}}', usedPercentage.toFixed(2)).replace('{{freeSpace}}', formatBytes(disk.freeSpace)).replace('{{totalSpace}}', formatBytes(disk.totalSpace));
         })
         .join('');
-
       this.contentElement.innerHTML = diskViewTemplate.replace(/<for>[\s\S]*?<\/for>/, allDisksHtml);
       this.contentElement.querySelectorAll('.cursor-pointer').forEach((element, index) => {
         element.addEventListener('dblclick', async () => await this.loadPath(disks[index].path));
@@ -124,7 +110,6 @@ class Tab {
       console.error('Error fetching disk data:', error);
     }
   }
-
   async loadFiles(path, addToHistory = true) {
     try {
       this.setTitle(path.split('\\').pop() || path);
@@ -133,20 +118,16 @@ class Tab {
       if (!filesResponse.ok || !listTemplateResponse.ok) {
         throw new Error('Failed to load file data or template');
       }
-
       const data = await filesResponse.json();
       const allItems = [...data.directories, ...data.files];
       const listTemplate = await listTemplateResponse.text();
-
       const parser = new DOMParser();
       const doc = parser.parseFromString(listTemplate, 'text/html');
       this.contentElement.innerHTML = '';
       this.contentElement.appendChild(doc.querySelector('.w-full'));
-
       const fileListContainer = this.contentElement.querySelector('.divide-y');
       const fileTemplate = fileListContainer.querySelector('for').innerHTML.trim();
       fileListContainer.innerHTML = '';
-
       allItems.forEach((file) => {
         let fileElementHtml = fileTemplate
           .replace('{{icon}}', file.isSymbolicLink ? 'link' : file.isDirectory ? 'folder' : 'description')
@@ -155,11 +136,9 @@ class Tab {
           .replace('{{fileName}}', file.name)
           .replace('{{lastModified}}', new Date(file.lastModified).toLocaleString())
           .replace('{{fileSize}}', file.isDirectory ? '' : formatBytes(file.size));
-
         const tempDiv = document.createElement('div');
         tempDiv.innerHTML = fileElementHtml;
         const fileElement = tempDiv.firstElementChild;
-
         if (file.isDirectory) {
           fileElement.addEventListener('dblclick', async () => await this.loadPath(file.path));
         }
@@ -179,7 +158,6 @@ class Tab {
     }
   }
 }
-
 class TabManager {
   constructor() {
     this.tabs = {};
@@ -189,20 +167,16 @@ class TabManager {
     this.contentContainer = document.getElementById('content-container');
     this.addTabButton = document.getElementById('add-tab-button');
     this.pathInput = document.getElementById('path-input');
-
     this.historyBackButton = document.getElementById('history-back-button');
     this.historyForwardButton = document.getElementById('history-forward-button');
-
     this.initEventListeners();
   }
-
   initEventListeners() {
     this.addTabButton.addEventListener('click', () => this.addTab());
     document.getElementById('home-button').addEventListener('click', () => this.getActiveTab()?.loadPath('此电脑'));
     document.getElementById('back-button').addEventListener('click', () => {
       const currentPath = this.pathInput.value;
       const lastSlashIndex = currentPath.lastIndexOf('\\');
-
       if (lastSlashIndex < 3 && currentPath.includes(':\\')) {
         this.getActiveTab()?.loadPath('此电脑');
       } else {
@@ -214,17 +188,14 @@ class TabManager {
         }
       }
     });
-
     this.historyBackButton.addEventListener('click', () => this.getActiveTab()?.goBack());
     this.historyForwardButton.addEventListener('click', () => this.getActiveTab()?.goForward());
-
     document.getElementById('refresh-button').addEventListener('click', () => {
       const currentPath = this.pathInput.value;
       if (currentPath && currentPath !== '此电脑') {
         this.getActiveTab()?.loadPath(currentPath);
       }
     });
-
     this.pathInput.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
         const newPath = this.pathInput.value.trim();
@@ -232,7 +203,6 @@ class TabManager {
       }
     });
   }
-
   addTab() {
     const tab = new Tab(this);
     this.tabs[tab.id] = tab;
@@ -241,19 +211,16 @@ class TabManager {
     this.switchTab(tab.id);
     tab.loadPath('此电脑');
   }
-
   switchTab(tabId) {
     if (this.activeTabId && this.tabs[this.activeTabId]) {
       this.tabs[this.activeTabId].hide();
     }
     this.activeTabId = tabId;
-
     const index = this.visitHistory.indexOf(tabId);
     if (index > -1) {
       this.visitHistory.splice(index, 1);
     }
     this.visitHistory.push(tabId);
-
     this.tabs[tabId].show();
     this.updateNavigationButtons();
     const activeTab = this.getActiveTab();
@@ -261,18 +228,15 @@ class TabManager {
       this.setPathInputValue(activeTab.history[activeTab.historyIndex]);
     }
   }
-
   closeTab(tabId) {
     const tab = this.tabs[tabId];
     tab.element.remove();
     tab.contentElement.remove();
     delete this.tabs[tabId];
-
     const historyIndex = this.visitHistory.indexOf(tabId);
     if (historyIndex > -1) {
       this.visitHistory.splice(historyIndex, 1);
     }
-
     if (this.activeTabId === tabId) {
       if (this.visitHistory.length > 0) {
         const newActiveTabId = this.visitHistory[this.visitHistory.length - 1];
@@ -284,7 +248,6 @@ class TabManager {
       }
     }
   }
-
   updateNavigationButtons() {
     const tab = this.getActiveTab();
     if (tab) {
@@ -295,40 +258,32 @@ class TabManager {
       this.historyForwardButton.disabled = true;
     }
   }
-
   getActiveTab() {
     return this.activeTabId ? this.tabs[this.activeTabId] : null;
   }
-
   setPathInputValue(value) {
     this.pathInput.value = value;
   }
 }
-
 document.addEventListener('DOMContentLoaded', () => {
   const tabManager = new TabManager();
   tabManager.addTab();
-
   const tooltip = document.getElementById('tooltip');
   const buttonsWithTooltip = document.querySelectorAll('[data-tooltip]');
-
-  buttonsWithTooltip.forEach(button => {
+  buttonsWithTooltip.forEach((button) => {
     button.addEventListener('mouseenter', () => {
       tooltip.textContent = button.dataset.tooltip;
       tooltip.classList.remove('hidden');
     });
-
     button.addEventListener('mousemove', (e) => {
       tooltip.style.left = `${e.pageX + 10}px`;
       tooltip.style.top = `${e.pageY + 10}px`;
     });
-
     button.addEventListener('mouseleave', () => {
       tooltip.classList.add('hidden');
     });
   });
 });
-
 function formatBytes(bytes, decimals = 2) {
   if (bytes === 0) return '0 Bytes';
   const k = 1024;
