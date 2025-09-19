9ddc232d522ccb49e2e0ba8923fdfd1bf8f68279
XiaoYingYo <759852125@qq.com>
2025-09-19 09:29:27 +0800

    no message

--- src/main/java/com/XiaoYing/controller/ApiController.java
+++ src/main/java/com/XiaoYing/controller/ApiController.java
@@ -2,13 +2,14 @@ package com.XiaoYing.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    @GetMapping
+    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
     public String hello() {
         return "Hello from API";
     }
