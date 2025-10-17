e88b34c3c13159a3631a37fde2fd2e840fda81b8
XiaoYingYo <759852125@qq.com>
2025-10-18 01:16:52 +0800

    no message

--- pack.bat
+++ pack.bat
@@ -3,7 +3,7 @@ chcp 65001
 setlocal enabledelayedexpansion
 cd /d %~dp0
 set "zipname=FileBrowser.zip"
-bz.exe c -r -ex:"%zipname%;target;.git" "%zipname%" *
+bz.exe c -r -ex:"%zipname%;target;.git;*\node_modules" "%zipname%" *
 :: 检查是否成功
 if %errorlevel% equ 0 (
     echo 打包成功:%zipname%
