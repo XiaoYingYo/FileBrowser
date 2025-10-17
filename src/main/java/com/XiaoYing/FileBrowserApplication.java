4ec8981117e2238c7bbde79d746b816ba11418ee
My Name <my-name@chromium.org>
2025-10-17 22:35:17 +0800

    ()

--- src/main/java/com/XiaoYing/FileBrowserApplication.java
+++ src/main/java/com/XiaoYing/FileBrowserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FileBrowserApplication {
     public static void main(String[] args) {
+        System.out.println("http://127.0.0.1:2666/");
         SpringApplication.run(FileBrowserApplication.class, args);
     }
 }
